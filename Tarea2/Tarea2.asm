9f547b3c6f55f2f7c6b14b7980b8ec33f15de553
yisslu <lugohalo999@gmail.com>
2023-03-14 14:05:31 -0600

    Primer programa

---
+++ Tarea2/Tarea2.asm
@@ -0,0 +1,38 @@
+processor 16f887
+include <p16f887.inc>
+valor1 equ h'21'
+valor2 equ h'22'
+valor3 equ h'23'
+cte1 equ 20h
+cte2 equ 50h
+cte3 equ 60h
+ 
+ ORG 0
+GOTO INICIO
+ ORG 5
+INICIO:BSF STATUS,RP0
+ BCF STATUS,RP1
+ MOVLW H'0'
+ MOVWF TRISB
+ BCF STATUS,RP0
+ CLRF PORTB 
+loop2 BSF PORTB,0
+ CALL retardo
+ BCF PORTB,0
+ CALL retardo
+ GOTO loop2
+ 
+retardo MOVLW cte1
+ MOVWF valor1
+tres MOVLW cte2
+MOVWF valor2
+dos MOVLW cte3
+MOVWF valor3
+uno DECFSZ valor3
+ GOTO uno
+DECFSZ valor2
+GOTO dos
+DECFSZ valor1
+GOTO tres
+RETURN
+END
